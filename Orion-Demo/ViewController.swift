9b387087a0ba83565f3d1e20b6cbbe574b31accb
Ricky Kresslein <ricky@unobserved.io>
2024-10-20 13:50:07 +0200

    Initial Commit

---
+++ Orion-Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Orion-Demo
+//
+//  Created by Ricky Kresslein on 10/20/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

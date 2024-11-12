24fe4fafa62263b4c2282b6165e57a1351f7712a
Ricky Kresslein <ricky@unobserved.io>
2024-11-12 14:53:38 +0100

    Tabs now start at max width (improved animation)

--- Orion-Demo/Tab.swift
+++ Orion-Demo/Tab.swift
@@ -2,13 +2,15 @@ import Cocoa
 import WebKit
 
 class Tab {
+	static let defaultTitle = String(repeating: " ", count: 35)
+
 	var url: URL
 	var title: String
 	var favicon: NSImage?
 	var webView: WKWebView
 	var titleObserver: NSKeyValueObservation?
 
-	init(url: URL, title: String = "", favicon: NSImage? = nil) {
+	init(url: URL, title: String = Tab.defaultTitle, favicon: NSImage? = nil) {
 		self.url = url
 		self.title = title
 		self.favicon = favicon
@@ -17,7 +19,7 @@ class Tab {
 		self.webView = WKWebView(frame: .zero, configuration: configuration)
 
 		self.titleObserver = self.webView.observe(\.title) { [weak self] webView, _ in
-			self?.title = webView.title ?? ""
+			self?.title = webView.title ?? Tab.defaultTitle
 			NotificationCenter.default.post(name: .tabTitleDidChange, object: self)
 		}
 
